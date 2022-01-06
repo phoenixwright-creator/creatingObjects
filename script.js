54ccfdf0ccc954023fa8bbef9f14215fead20ac7
phoenixwright-creator <phoenixwright725@gmail.com>
2022-01-06 14:46:45 +0100

    added functionality to render the books in the library as an unordered list. added a button to add new books, and render the whole library on the screen

--- script.js
+++ script.js
@@ -1,54 +1,68 @@
 let myLibrary = [];
 
-function Book () {
-};
+const libraryTab = document.getElementById('libraryTab');
 
 function Book (title, author, pages, read) {
-    this.title = title,
-    this.author = author,
-    this.pages = pages,
-    this.read = read
+  this.title = title,
+  this.author = author,
+  this.pages = pages,
+  this.read = read
 };
 
 Book.prototype.info = function () {
-    return this.title + ', by ' + this.author + ', ' + this.pages + ' pages, ' + this.read.toLowerCase() + '.';
+  return this.title + ', by ' + this.author + ', ' + this.pages + ' pages, ' + this.read.toLowerCase() + '.';
 };
 
 function addBookToLibrary() {
-    let title = prompt('Please enter the name of the book to add to the library : ');
-    let author = prompt('Please enter the name of its author : ');
-    let pages = prompt('Please enter the number of pages this book contains : ');
-    let read = prompt('Please enter if you read it or not (Y for Yes/N for No) : ');
-    if (read === 'Y'){
-        read = 'Already read';
-    }
-    else if (read === 'N'){
-        read = 'Not read yet';
-    }
-    else {
-        read = 'No information about reading status yet';
-    }
-    if (title !== null && author !== null && pages !== null){
-        let book = new Book(title, author, pages, read);
-        myLibrary.push(book);
-    }
-    
+  const title = prompt('Please enter the name of the book to add to the library : ');
+  const author = prompt('Please enter the name of its author : ');
+  const pages = prompt('Please enter the number of pages this book contains : ');
+  let read = prompt('Please enter if you read it or not (Y for Yes/N for No) : ');
+  if(read === 'Y') {
+    read = 'Already read';
+  }
+  else if(read === 'N') {
+    read = 'Not read yet';
+  }
+  else{
+    read = 'No information about reading status yet';
+  }
+  if (title !== null && author !== null && pages !== null) {
+    let book = new Book(title, author, pages, read);
+    myLibrary.push(book);
+  }
+  displayLibrary(myLibrary);
 }
 
-function displayLibrary(library){
-    if(library.length !== 0){
-        for (let book in library){
-            console.log(library[book].info());
-        }
-    }
-    else{
-        console.log('No book in the library.');
-    }
+function displayLibrary(library) {
+  const bookList = document.getElementById('bookList');
+  libraryTab.removeChild(bookList);
+  const newList = document.createElement('ul');
+  newList.id = 'bookList';
+  for (let book in library) {
+    const bookLine = document.createElement('li');
+    bookLine.id = 'book' + (Number(book) + 1);
+    bookLine.innerHTML = library[book].info();
+    newList.appendChild(bookLine);
+  }
+  libraryTab.appendChild(newList);
 }
 
-addBookToLibrary();
+const addBookButton = document.createElement('button');
+addBookButton.innerHTML = 'Add a new book';
+addBookButton.id = 'addBookButton';
 
-displayLibrary(myLibrary);
+libraryTab.appendChild(addBookButton);
+
+const list = document.createElement('ul');
+list.id = 'bookList';
+const line = document.createElement('li');
+line.id = 'defaultLine';
+line.innerHTML = 'No books yet';
+list.appendChild(line);
+libraryTab.appendChild(list);
+
+addBookButton.addEventListener('click', addBookToLibrary);
 
 /*
 
